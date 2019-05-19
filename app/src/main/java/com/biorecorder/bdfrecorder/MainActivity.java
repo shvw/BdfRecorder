0af76af5390be606635073ab9cc83eed3be9df87
alvw <flashvw@gmail.com>
2019-05-19 19:54:04 +0300

    Added file writing permission.

--- app/src/main/java/com/biorecorder/bdfrecorder/MainActivity.java
+++ app/src/main/java/com/biorecorder/bdfrecorder/MainActivity.java
@@ -1,23 +1,36 @@
 package com.biorecorder.bdfrecorder;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements ServiceConnection {
-
-    private BdfRecorderService bdfRecorderService;
+     private BdfRecorderService bdfRecorderService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkAndRequestFileWritingPermision();
         setContentView(R.layout.activity_main);
+    }
+
+    private void checkAndRequestFileWritingPermision() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.e("xxx","permission not granted");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 13);
+        }
     }
 
     public void onStartButtonClick(View view){
